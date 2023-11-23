1fc586ca324ddd13988e63595a8ab1c216be9c0d
dharabooth <146215658+dharabooth@users.noreply.github.com>
2023-11-23 21:07:22 +0000

    debug

---
+++ app/controllers/division_controller.rb
@@ -0,0 +1,15 @@
+class DivisionController < ApplicationController
+
+  def show_division_form
+    render({ :template => "div_templates/division_form" })
+  end
+
+  def divide_these
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+  
+    @result = @first_num / @second_num
+
+    render({ :template => "div_templates/divide_results" })
+  end
+end
